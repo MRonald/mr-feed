af7fa00c74abe6e6324d161c6ee4c86062ae8749
m_ronald <michael.ronald@allycode.com.br>
2022-12-03 11:10:04 -0300

    init style from main block | sidebar component | install icon library

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
 
+import styles from './App.module.css';
 import './global.css';
 
 function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Michael Ronald"
-        content="Lorem ipsum"
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Michael Ronald"
+            content="Lorem ipsum"
+          />
+        </main>
+      </div>
     </div>
   )
 }
